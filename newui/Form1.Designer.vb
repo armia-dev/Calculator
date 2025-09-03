30213c806f6758616fb44a80d645847edfa4fdb9
Armia <Armia@DESKTOP-EJ2JS9N>
2025-09-03 23:29:09 +0800

    Add project files.

---
+++ newui/Form1.Designer.vb
@@ -0,0 +1,295 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        btn1 = New Button()
+        btn2 = New Button()
+        btnCE = New Button()
+        btnDel = New Button()
+        btn3 = New Button()
+        btnAdd = New Button()
+        btnSub = New Button()
+        btnMul = New Button()
+        btnDiv = New Button()
+        btn6 = New Button()
+        btn5 = New Button()
+        btn9 = New Button()
+        btn8 = New Button()
+        btn4 = New Button()
+        btn7 = New Button()
+        btn0 = New Button()
+        btnEquals = New Button()
+        btnDot = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = SystemColors.InactiveBorder
+        TextBox1.Dock = DockStyle.Top
+        TextBox1.Location = New Point(0, 0)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(308, 108)
+        TextBox1.TabIndex = 0
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' btn1
+        ' 
+        btn1.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn1.Location = New Point(9, 112)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(54, 54)
+        btn1.TabIndex = 1
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn2.Location = New Point(69, 112)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(54, 54)
+        btn2.TabIndex = 2
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btnCE
+        ' 
+        btnCE.BackColor = Color.Red
+        btnCE.Font = New Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnCE.Location = New Point(249, 232)
+        btnCE.Name = "btnCE"
+        btnCE.Size = New Size(49, 114)
+        btnCE.TabIndex = 3
+        btnCE.Text = "CE"
+        btnCE.UseVisualStyleBackColor = False
+        ' 
+        ' btnDel
+        ' 
+        btnDel.BackColor = Color.Tomato
+        btnDel.Font = New Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnDel.Location = New Point(249, 112)
+        btnDel.Name = "btnDel"
+        btnDel.Size = New Size(49, 114)
+        btnDel.TabIndex = 4
+        btnDel.Text = "Del"
+        btnDel.UseVisualStyleBackColor = False
+        ' 
+        ' btn3
+        ' 
+        btn3.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn3.Location = New Point(129, 112)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(54, 54)
+        btn3.TabIndex = 5
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.BackColor = SystemColors.ControlLight
+        btnAdd.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnAdd.Location = New Point(189, 112)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(54, 54)
+        btnAdd.TabIndex = 6
+        btnAdd.Text = "+"
+        btnAdd.UseVisualStyleBackColor = False
+        ' 
+        ' btnSub
+        ' 
+        btnSub.BackColor = SystemColors.ControlLight
+        btnSub.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnSub.Location = New Point(189, 175)
+        btnSub.Name = "btnSub"
+        btnSub.Size = New Size(54, 54)
+        btnSub.TabIndex = 7
+        btnSub.Text = "-"
+        btnSub.UseVisualStyleBackColor = False
+        ' 
+        ' btnMul
+        ' 
+        btnMul.BackColor = SystemColors.ControlLight
+        btnMul.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnMul.Location = New Point(189, 235)
+        btnMul.Name = "btnMul"
+        btnMul.Size = New Size(54, 54)
+        btnMul.TabIndex = 8
+        btnMul.Text = "x"
+        btnMul.UseVisualStyleBackColor = False
+        ' 
+        ' btnDiv
+        ' 
+        btnDiv.BackColor = SystemColors.ControlLight
+        btnDiv.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnDiv.Location = New Point(189, 292)
+        btnDiv.Name = "btnDiv"
+        btnDiv.Size = New Size(54, 54)
+        btnDiv.TabIndex = 9
+        btnDiv.Text = "÷"
+        btnDiv.UseVisualStyleBackColor = False
+        ' 
+        ' btn6
+        ' 
+        btn6.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn6.Location = New Point(129, 175)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(54, 54)
+        btn6.TabIndex = 10
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn5.Location = New Point(69, 175)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(54, 54)
+        btn5.TabIndex = 11
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn9.Location = New Point(129, 235)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(54, 54)
+        btn9.TabIndex = 12
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn8.Location = New Point(69, 232)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(54, 54)
+        btn8.TabIndex = 13
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn4.Location = New Point(9, 175)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(54, 54)
+        btn4.TabIndex = 14
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn7.Location = New Point(9, 232)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(54, 54)
+        btn7.TabIndex = 15
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn0.Location = New Point(69, 292)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(54, 54)
+        btn0.TabIndex = 16
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' btnEquals
+        ' 
+        btnEquals.BackColor = SystemColors.ActiveCaption
+        btnEquals.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnEquals.Location = New Point(129, 292)
+        btnEquals.Name = "btnEquals"
+        btnEquals.Size = New Size(54, 54)
+        btnEquals.TabIndex = 17
+        btnEquals.Text = "="
+        btnEquals.UseVisualStyleBackColor = False
+        ' 
+        ' btnDot
+        ' 
+        btnDot.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnDot.Location = New Point(9, 292)
+        btnDot.Name = "btnDot"
+        btnDot.Size = New Size(54, 54)
+        btnDot.TabIndex = 18
+        btnDot.Text = "."
+        btnDot.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlLightLight
+        ClientSize = New Size(308, 359)
+        Controls.Add(btnDot)
+        Controls.Add(btnEquals)
+        Controls.Add(btn0)
+        Controls.Add(btn7)
+        Controls.Add(btn4)
+        Controls.Add(btn8)
+        Controls.Add(btn9)
+        Controls.Add(btn5)
+        Controls.Add(btn6)
+        Controls.Add(btnDiv)
+        Controls.Add(btnMul)
+        Controls.Add(btnSub)
+        Controls.Add(btnAdd)
+        Controls.Add(btn3)
+        Controls.Add(btnDel)
+        Controls.Add(btnCE)
+        Controls.Add(btn2)
+        Controls.Add(btn1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btnCE As Button
+    Friend WithEvents btnDel As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSub As Button
+    Friend WithEvents btnMul As Button
+    Friend WithEvents btnDiv As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnEquals As Button
+    Friend WithEvents btnDot As Button
+
+End Class
---
+++ newui/Form1.vb
@@ -0,0 +1,133 @@
+﻿Public Class Form1
+    Dim currentInput As String = ""
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.KeyPreview = True
+        TextBox1.TabStop = False
+        TextBox1.ReadOnly = True
+    End Sub
+
+    ' Updator
+    Private Sub UpdateDisplay()
+        TextBox1.Text = currentInput
+    End Sub
+
+    ' Buttons
+    Private Sub Button_Click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click,
+        btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click, btnAdd.Click, btnSub.Click, btnMul.Click, btnDiv.Click, btnDot.Click
+
+        Dim btn As Button = CType(sender, Button)
+        Dim value As String = btn.Text
+
+        If "0123456789".Contains(value) Then
+            currentInput &= value
+
+        ElseIf value = "." Then
+            Dim lastNumber = GetLastNumber(currentInput)
+            If Not lastNumber.Contains(".") Then
+                currentInput &= "."
+            End If
+
+        ElseIf "+-x÷".Contains(value) Then
+            If currentInput = "" OrElse "+-x÷".Contains(currentInput.Last()) Then Exit Sub
+            currentInput &= value
+        End If
+
+        UpdateDisplay()
+    End Sub
+
+    ' Extract
+    Private Function GetLastNumber(expr As String) As String
+        Dim ops = "+-x÷"
+        Dim lastOpIndex = expr.LastIndexOfAny(ops.ToCharArray())
+        If lastOpIndex = -1 Then Return expr
+        Return expr.Substring(lastOpIndex + 1)
+    End Function
+
+    ' Equal
+    Private Sub btnEquals_Click(sender As Object, e As EventArgs) Handles btnEquals.Click
+        Try
+            Dim expr As String = currentInput.Replace("x", "*").Replace("÷", "/")
+            Dim result = New DataTable().Compute(expr, Nothing)
+            currentInput = result.ToString()
+        Catch ex As Exception
+            currentInput = "Error"
+        End Try
+
+        UpdateDisplay()
+    End Sub
+
+    ' Clear all input
+    Private Sub btnCE_Click(sender As Object, e As EventArgs) Handles btnCE.Click
+        currentInput = ""
+        UpdateDisplay()
+    End Sub
+
+    ' Delete Button
+    Private Sub btnDel_Click(sender As Object, e As EventArgs) Handles btnDel.Click
+        If currentInput.Length > 0 Then
+            currentInput = currentInput.Substring(0, currentInput.Length - 1)
+            UpdateDisplay()
+        End If
+    End Sub
+
+    ' Keyboard input
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Me.KeyPreview = True
+        e.SuppressKeyPress = True
+
+        Dim keyChar As String = ""
+
+        Select Case e.KeyCode
+            Case Keys.D0 To Keys.D9
+                keyChar = (e.KeyCode - Keys.D0).ToString()
+            Case Keys.NumPad0 To Keys.NumPad9
+                keyChar = ChrW(e.KeyCode - Keys.NumPad0 + Asc("0"))
+
+            Case Keys.Add, Keys.Oemplus
+                keyChar = "+"
+            Case Keys.Subtract, Keys.OemMinus
+                keyChar = "-"
+            Case Keys.Multiply
+                keyChar = "x"
+            Case Keys.Divide, Keys.OemQuestion
+                keyChar = "÷"
+
+            Case Keys.Decimal, Keys.OemPeriod
+                Dim lastNumber = GetLastNumber(currentInput)
+                If Not lastNumber.Contains(".") Then
+                    keyChar = "."
+                End If
+
+            Case Keys.OemOpenBrackets
+                keyChar = "("
+            Case Keys.Oem6
+                keyChar = ")"
+
+            Case Keys.C
+                btnCE.PerformClick()
+                Exit Sub
+            Case Keys.Enter, Keys.Oemplus
+                btnEquals.PerformClick()
+                Exit Sub
+            Case Keys.Back
+                btnDel.PerformClick()
+                Exit Sub
+        End Select
+
+        ' Checker 
+        If "0123456789".Contains(keyChar) Then
+            currentInput &= keyChar
+        ElseIf keyChar = "." Then
+            currentInput &= keyChar
+        ElseIf "+-x÷".Contains(keyChar) Then
+            If currentInput = "" OrElse "+-x÷".Contains(currentInput.Last()) Then Exit Sub
+            currentInput &= keyChar
+        ElseIf "()".Contains(keyChar) Then
+            currentInput &= keyChar
+        End If
+
+        UpdateDisplay()
+
+
+    End Sub
+End Class
